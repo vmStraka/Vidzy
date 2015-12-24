b54516ed769c93b29bba115f1818824bedf73aeb
Sihang Shao <sihang.shao@sap.com>
2015-12-24 14:54:22 +0800

    use angularjs to comsume restful API and create controller for home page

---
+++ public/javascripts/vidzy.js
@@ -0,0 +1,20 @@
+var app = angular.module('Vidzy', ['ngResource', 'ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
+
+app.controller('HomeCtrl', ['$scope', '$resource', 
+    function($scope, $resource){
+        var Videos = $resource('/api/videos');
+        Videos.query(function(videos){
+            $scope.videos = videos;
+        });
+    }]);
