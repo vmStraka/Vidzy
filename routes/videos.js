e722f14a33eb84d8b162a084213c4c7758c2dcdd
Sihang Shao <sihang.shao@sap.com>
2015-12-24 13:46:03 +0800

    add router for /api/videos to return json

---
+++ routes/videos.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('localhost:27017/Vidzy');
+
+router.get('/', function(req, res) {
+    var collection = db.get('videos');
+    collection.find({}, function(err, videos) {
+        if (err) {
+            throw err;
+        }
+        res.json(videos);
+    });
+});
+
+module.exports = router;
